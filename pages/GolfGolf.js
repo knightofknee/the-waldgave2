1d3968f598018d5e900a41d21b53c6e3c1b13549
Brian Carlisle <bricarlis@gmail.com>
2023-03-26 23:58:03 -0500

    goo

--- pages/GolfGolf.js
+++ pages/GolfGolf.js
@@ -4,8 +4,69 @@ export default function GolfGolf () {
 <div>
   <div id="newOuterShell" style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
     <p>
-      GolfGolf mini-golf. All about the golf, profit capped at 1% or something. Over is used to cover costs of upgrades and to lower future prices. Every hole is a banger, both a unique golf challenge and unique theme. Black curtains between. It is byob and has a bar with at-cost+1$ beers and wine. Bartender can charge whatever they want for other things. Every hole has a target you can hit that gives you -1, making a perfect score near impossible. Highest level of investment: you never really wait because you spend set time on holes just watching what happens while you are there. Someone could be changing the hole while you are on it, like a an avalanche or slowly cracking ice. Wait times can also be reduced because you don't know your next hole until you finish your current one. This is so that you can be moved around a slow group and then go back to that hole later. Utilization is maxed and wait time is minimized. Also we can anticipate slowdowns and time out little mini art events. Profit is capped but costs are not. The art staff can choose whatever they find best and push barriers. They are golfgolf's core, the creators. (We could have a burlesque night where dancers dance among the holes, both fun and edgy for publicity).
+      GolfGolf mini-golf. All about the golf, profit capped at 1% or something. Over is used to cover costs of upgrades and to lower future prices. Every hole is a banger, both a unique golf challenge and unique theme. Black curtains between. It is byob and has a bar with at-cost+1$ beers and wine. Bartender can charge whatever they want for other things. Every hole has a target you can hit that gives you -1, making a perfect score near impossible. Highest level of investment: you never really wait because you spend set time on holes just watching what happens while you are there. Someone could be changing the hole while you are on it, like a an avalanche or slowly cracking ice. Wait times can also be reduced because you don't know your next hole until you finish your current one. This is so that you can be moved around a slow group and then go back to that hole later. Utilization is maxed and wait time is minimized. Also we can anticipate slowdowns and time out little mini art events. Profit is capped but costs are not. The art staff can choose whatever they find best and push barriers. They are golfgolf's core, the creators.
     </p>
+    <h2>GolfGolf mini-Golf Initial Hole Concepts</h2>
+    <h5>Front 9</h5>
+    <ol>
+      <li>
+        The Windmill. Unique challenge: timing. We begin with our take on a classic. Straight ahead of the start is a giant windmill with half of it being covered with a wheel of fortune. If you hit the jackpot, your ball gets caught on the wheel and lifted up to the first bonus point. This gives you -1 stroke and is only available on the first swing. Near misses at the jackpot get bumped back, and other parts could pick you up still and put you in a less advantageous position in part 2 of the hole. Further away but more directly towards the hole, there is a smaller, faster windmill. It is structured such that it funnels your ball and pads it to be forgiving, so if you miss the timing, you are guranteed to have a decent shot. The bonus point route requires aim and speed to be just right, and if you fail you are punished by being placed far away from the hole on part 2, the 'green'. (The definition of green here is wherever you can putt at the hole directly. Every hole here will have you start off the green. Some hole in one's will be possible, but they will more closer resemble the frequency of regular golf hole in one's) If you get past the small windmill, you are in an advantageous part of the green. You can't hit a hole in one, but you can get closer than where the bonus point drops off the ball, which pretty close(10ft?), with a straight shot and favorable backdrop.
+      </li>
+      <li>
+        Yeti Slopes ski hill. Unique challenge: putting downhill. Staying with the familiar with plinko here. There is a safer green route that bends down the mountain with curves that catch you. There is a blue route down through the trees, where you can end up on the green route, or you can continue all the way down the mountain in one go. Or you can go the black diamond route and aim for a skinny ramp that leads to the bonus point. Missing on the ramp puts you at a disadvantage compared to the plinko players on the green. Ideally this green is super fast, like it is ice.
+      </li>
+      <li>
+
+      </li>
+      <li>
+
+      </li>
+      <li>
+
+      </li>
+      <li>
+
+      </li>
+      <li>
+
+      </li>
+      <li>
+
+      </li>
+      <li>
+
+      </li>
+    </ol>
+    <h5>Back 9</h5>
+    <ol start={10}>
+      <li>
+      Hole where you can hit your ball into a ramp that goes horizontal to vertical along the wall and your ball goes into one of those maze boxes where you control horizontal and vertical, and there are holes to avoid. There is a bonus in there, and you can see where it will take you. Also different paths give you different number of strokes.
+      </li>
+      <li>
+
+      </li>
+      <li>
+
+      </li>
+      <li>
+
+      </li>
+      <li>
+
+      </li>
+      <li>
+
+      </li>
+      <li>
+
+      </li>
+      <li>
+
+      </li>
+      <li>
+
+      </li>
+    </ol>
   </div>
 
 <style jsx>
