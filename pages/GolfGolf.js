6ae0ae39b11fb013a971abfaa15202610fa32a12
Brian Carlisle <bricarlis@gmail.com>
2023-03-27 23:45:57 -0500

    goo

--- pages/GolfGolf.js
+++ pages/GolfGolf.js
@@ -2,10 +2,28 @@
 export default function GolfGolf () {
   return (
 <div>
-  <div id="newOuterShell" style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
+  <div id="newOuterShell" style={{display:'flex',justifyContent:'center',alignItems:'center', flexDirection:'column'}}>
     <p>
       GolfGolf mini-golf. All about the golf, profit capped at 1% or something. Over is used to cover costs of upgrades and to lower future prices. Every hole is a banger, both a unique golf challenge and unique theme. Black curtains between. It is byob and has a bar with at-cost+1$ beers and wine. Bartender can charge whatever they want for other things. Every hole has a target you can hit that gives you -1, making a perfect score near impossible. Highest level of investment: you never really wait because you spend set time on holes just watching what happens while you are there. Someone could be changing the hole while you are on it, like a an avalanche or slowly cracking ice. Wait times can also be reduced because you don't know your next hole until you finish your current one. This is so that you can be moved around a slow group and then go back to that hole later. Utilization is maxed and wait time is minimized. Also we can anticipate slowdowns and time out little mini art events. Profit is capped but costs are not. The art staff can choose whatever they find best and push barriers. They are golfgolf's core, the creators.
     </p>
+    <h2>GolfGolf Game Winning Concepts</h2>
+    <ol>
+      <li>
+        Every hole is a banger. no straight shot, no phoning it in. This is the foundation for our claim for best minigolf course in the galaxy.
+        </li>
+      <li>
+        Cheapest Beer in the City. We are all about the golf. So if we get a liquor license for a tap or for selling cans, we sell at a price to cover the cost of the beer plus one dollar. If someone sells cheaper than that, we undercut their price unless it takes us below cost. But if we can sell out of kegs, I don't think anyone would be anywhere close to us.
+      </li>
+      <li>
+        Your money goes towards better golf. My personal profit is publicly capped, so your money goes towards improving the course or paying the employees.
+      </li>
+      <li>
+        Every hole has a bonus point and unique golf challenge, so a perfect score is achievable but very difficult. (in full form, these trigger special effects)
+      </li>
+      <li>
+        Hole order is flexible and controlled so that you aren't needlessly waiting.
+      </li>
+    </ol>
     <h2>GolfGolf mini-Golf Initial Hole Concepts</h2>
     <h5>Front 9</h5>
     <ol>
