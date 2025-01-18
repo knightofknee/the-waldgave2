354fdc8e73f4de7e0c7efb087fd86bd9246e204d
Brian Carlisle <bricarlis@gmail.com>
2025-01-18 03:14:39 -0600

    goo

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html style={{backgroundColor: 'rgb(220, 170, 210)'}}>
-      <Head ><meta name="viewport" content="width=device-width, initial-scale=1"/> </Head>
+      <Head ><meta name="viewport" content="width=device-width, initial-scale=1"/>
+      <link rel="icon" href="/favicon.ico" />
+       </Head>
       <body>
         <Main />
         <NextScript />
